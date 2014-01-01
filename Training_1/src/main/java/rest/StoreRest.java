1a665499230d04605fda0b2ececc70e42676af82
vk <vkryz@softserveinc.com>
2014-01-02 00:28:23 +0200

    Added test bean mailSender

--- Training_1/src/main/java/rest/StoreRest.java
+++ Training_1/src/main/java/rest/StoreRest.java
@@ -1,6 +1,7 @@
 package rest;
 
 import java.util.List;
+import java.util.Properties;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -12,7 +13,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.sun.corba.se.impl.logging.ORBUtilSystemException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import dto.ProductDto;
@@ -55,6 +63,34 @@ public class StoreRest {
     		status =Response.Status.ACCEPTED;
     	}
         return Response.status(status).build();
+    }
+
+    @Bean
+    private boolean mailSender(){
+        boolean isSent = true;
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.mail.ru");
+        javaMailSender.setPort(465);
+        javaMailSender.setUsername("kvm84@mail.ru");
+        javaMailSender.setPassword("password");
+        javaMailSender.setProtocol("smtps");
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtps.auth", "true");
+        properties.setProperty("mail.smtps.starttls.enable", "true");
+        javaMailSender.setJavaMailProperties(properties);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("kvm84@mail.ru");
+        message.setFrom("kvm84@mail.ru");
+        message.setSubject("Subject");
+        message.setText("Hello! This is my first message over Spring Mail");
+        try{
+            javaMailSender.send(message);
+            System.out.println("Mail sent successful");
+        } catch (MailException exception) {
+            System.out.println("Some errors was found" + exception.getStackTrace());
+            isSent = false;
+        }
+        return isSent;
     }
 
 }
